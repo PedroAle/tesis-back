18e248f4177ee30a6758e976bb5e93f55060b215
Javier Stifano <jstifano18@gmail.com>
2019-12-18 02:26:32 -0400

    Servicios para actualizar y eliminar un periodo academico

--- api/terms/services.py
+++ api/terms/services.py
@@ -29,3 +29,17 @@ def crear_term(value):
     sql = "INSERT INTO api_term(term) VALUES ('{}')".format(value)
     cur.execute(sql)
     conn.commit()
+
+def actualizar_term(data):
+    conn = create_connection('db.sqlite3')
+    cur = conn.cursor()
+    sql = "UPDATE api_term SET term='{0}' WHERE id={1}".format(data['term'], data['id'])
+    cur.execute(sql)
+    conn.commit()
+
+def eliminar_term(id):
+    conn = create_connection('db.sqlite3')
+    cur = conn.cursor()
+    sql = "DELETE FROM api_term WHERE id={0}".format(id)
+    cur.execute(sql)
+    conn.commit()
--- api/terms/views.py
+++ api/terms/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from api.terms import services
-from api.services.utils import isEmpty, generateError, successAction
+from api.services.utils import isEmpty, generateError, successAction, isNumber 
 import json
 
 # Create your views here.
@@ -14,10 +13,30 @@ def create_terms(request):
     if(request.method == "POST"):
         term = json.loads(request.body)['term']
         if( isEmpty(term) ):
-            return JsonResponse(generateError(400, 'Ingrese un TERM valido.'))
+            return JsonResponse(generateError(400, 'Parámetros inválidos.'))
         else: 
             services.crear_term(term)
             return JsonResponse(successAction(200, 'Se creo exitosamente'))
     else:
         return JsonResponse(generateError(401, 'Método HTTP inválido'))
-    
+
+def update_term(request):
+    if(request.method == "PUT"):
+        data = json.loads(request.body)['data']
+        if( isEmpty(data['id']) or isEmpty(data['terms']) or (not isNumber(data['id'])) ):
+            return JsonResponse(generateError(400, 'Parámetros inválidos.'))
+        else: 
+            services.actualizar_term(data)
+            return JsonResponse(successAction(200, 'Se actualizó el período exitosamente'))
+    else:
+        return JsonResponse(generateError(401, 'Método HTTP inválido'))
+
+def delete_term(request, id):
+    if(request.method == "GET"):
+        if( isEmpty(id) or (not isNumber(id)) ):
+            return JsonResponse(generateError(400, 'Parámetros inválidos.'))
+        else: 
+            services.eliminar_term(id)
+            return JsonResponse(successAction(200, 'Se eliminó el período exitosamente'))
+    else:
+        return JsonResponse(generateError(401, 'Método HTTP inválido'))
