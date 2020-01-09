157fe16917ea82ce146e85805e3eec58b70d7f18
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2020-01-09 16:40:43 -0400

    agregando al delete la tabla correcciones

--- delete.py
+++ delete.py
@@ -7,6 +7,11 @@ import json
 def delete_data():
     conn = create_connection('db.sqlite3')
     cur = conn.cursor()
+
+    sql = '''Delete from api_correcciones'''
+    cur.execute(sql)
+    conn.commit()
+
     sql = '''Delete from api_defensa'''
     cur.execute(sql)
     conn.commit()
